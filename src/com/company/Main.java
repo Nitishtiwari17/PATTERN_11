4dc712597c67e03526d648cf818b6d127667a853
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2022-01-06 09:51:58 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=10;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<=10;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	// write your code here
+    }
+}
